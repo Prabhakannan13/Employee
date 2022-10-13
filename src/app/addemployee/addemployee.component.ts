c715a4970d2c12a250ce1a2b57f2b8ee79f13746
Prabhakannan13 <prabhasanthiya2001@gmail.com>
2022-10-13 23:16:59 +0530

    pushchange

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -9,6 +9,38 @@ export class AddemployeeComponent implements OnInit {
 
   constructor() { }
 
+      empcode=""
+      empname=""
+      designation=""
+      gender=""
+      companyname=""
+      salary=""
+      address=""
+      mobilenumber=""
+      companyemailid=""
+      yearofexperience=""
+      bloodgroup=""
+      dob=""
+
+      readValues=()=>{
+        let data={
+          "empcode":this.empcode,
+          "empname":this.empname,
+          "designation":this.designation,
+          "gender":this.gender,
+          "companyname":this.companyname,
+          "salary":this.salary,
+          "address":this.address,
+          "mobilenumber":this.mobilenumber,
+          "companyemailid":this.companyemailid,
+          "yearofexperience":this.yearofexperience,
+          "bloodgroup":this.bloodgroup,
+          "dob":this.dob
+        }
+        console.log(data)
+      }
+
+
   ngOnInit(): void {
   }
 
